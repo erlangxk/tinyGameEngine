333df6ae68affc6d28d3ea5e21833bec0d621c92
erlangxk <simon.xikun@gmail.com>
2019-06-11 15:29:57 +0800

    change it to a web project

---
+++ hello_world/lib/asset_manager.dart
@@ -0,0 +1,61 @@
+import 'dart:html';
+import 'dart:convert';
+import 'dart:async';
+
+int add(int a, int b)=> a+b;
+
+abstract class Event {
+  final String url;
+  const Event(this.url);
+
+  bool isComplete();
+  double percentage();
+}
+
+class OngoingEvent extends Event {
+  final int total;
+  final int loaded;
+  final bool computable;
+
+  const OngoingEvent(String url, this.total, this.loaded,this.computable):super(url);
+
+  double percentage() =>computable?loaded/total: null;
+
+  bool isComplete() => computable && total == loaded;     
+}
+
+class CompleteEvent<T> extends Event {
+  final T data;
+  const CompleteEvent(String url, this.data):super(url);
+
+  bool isComplete() => true;
+
+  double percentage() => 1.0;
+}
+
+class ErrorEvent<E> extends Event {
+  final E error;
+  const ErrorEvent(String url, this.error):super(url);
+  
+  bool isComplete() =>false;
+  double percentage() => null;
+}
+
+Stream<Event> loadJson(String url) {
+      var sc = StreamController<Event>();
+
+      void startLoading(){
+        HttpRequest.getString(url, onProgress: (event){
+          sc.add(OngoingEvent(url, event.total, event.loaded,event.lengthComputable));
+        }).then((js){
+          sc.add(CompleteEvent(url,json.decode(js)));
+          sc.close();
+        }).catchError((err){
+          sc.add(ErrorEvent(url, err));
+          sc.close();
+        });
+      }
+
+      sc.onListen =  startLoading;
+      return sc.stream;
+}
---
+++ hello_world/web/main.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+import 'package:hello_world/asset_manager.dart';
+
+void main() {
+  int result = add(3,4);
+  querySelector('#output').text = 'Your Dart app is running. ${result}';
+  print("aaaaaaaaaaaa");
+}
