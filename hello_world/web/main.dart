19d263a798fd0ff1ab3a154ef5fc02239d07049d
erlangxk <simon.xikun@gmail.com>
2019-06-14 11:25:40 +0800

    game1

--- hello_world/web/main.dart
+++ hello_world/web/main.dart
@@ -1,13 +1,15 @@
 import 'dart:html';
-import 'package:gamedev/game1.dart';
+import 'package:gamedev/game1.dart' as game1;
 
-
-void main() {
+void runGame1(){
   CanvasElement canvas = querySelector("#canvas");
   CanvasRenderingContext2D ctx = canvas.getContext('2d');
-  Keyboard keyboard = Keyboard();
-  var game=Game(canvas.width, canvas.height, ctx);
+  game1.Keyboard keyboard = game1.Keyboard();
+  var game = game1.Game(canvas.width, canvas.height, ctx);
   game.run(keyboard);
+}
 
+void main() {
+  runGame1();
 }
 
